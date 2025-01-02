464d49efda70603f53f1f9256078a895c740824e
Sitaratoons <mrajayh393798@gmail.com>
2025-01-02 13:07:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,12 @@ import asyncio, datetime, time
 ACCEPTED_TEXT = "{user},\n\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝗌𝗍 𝖳𝗈 𝖩𝗈𝗂𝗇 {chat} 𝖺𝗌 𝖻𝖾𝖾𝗇 𝖠𝖼𝖼𝖾𝗉𝗍𝖾𝖽."
 START_TEXT = "{},\n\n𝖨 𝖼𝖺𝗇 𝖺𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝖼𝖺𝗅𝗅𝗒 𝖺𝗉𝗉𝗋𝗈𝗏𝖾 𝗎𝗌𝖾𝗋𝗌 𝗂𝗇 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌.\n\n𝖩𝗎𝗌𝗍 𝖺𝖽𝖽 𝗆𝖾 𝗂𝗇 𝗒𝗈𝗎𝗋 𝖼𝗁𝖺𝗇𝗇𝖾𝗅𝗌 𝖺𝗇𝖽 𝗀𝗋𝗈𝗎𝗉𝗌 𝗐𝗂𝗍𝗁 𝗉𝖾𝗋𝗆𝗂𝗌𝗌𝗂𝗈𝗇 𝗍𝗈 𝖺𝖽𝖽 𝗇𝖾𝗐 𝗆𝖾𝗆𝖻𝖾𝗋𝗌.\n\n**<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : @AcxAnime</blockquote>**"
 
-API_ID = int(env.get('API_ID', ''))
-API_HASH = env.get('API_HASH', '')
+API_ID = int(env.get('API_ID', '18946488'))
+API_HASH = env.get('API_HASH', 'c163d4e28e63196c3806cf3b9b2885de')
 BOT_TOKEN = env.get('BOT_TOKEN', '')
-DB_URL = env.get('DB_URL', '')
-ADMINS = int(env.get('ADMINS', ''))
-AUTH_CHANNEL = int(env.get('AUTH_CHANNEL', ''))
+DB_URL = env.get('DB_URL', 'mongodb+srv://stoons:stoons@ajay.v5uug.mongodb.net/?retryWrites=true&w=majority')
+ADMINS = int(env.get('ADMINS', '6692613520'))
+AUTH_CHANNEL = int(env.get('AUTH_CHANNEL', '-1002355394644'))
 
 Dbclient = AsyncIOMotorClient(DB_URL)
 Cluster = Dbclient['Cluster0']
